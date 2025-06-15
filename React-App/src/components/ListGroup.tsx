963b1398059fb9281b7d461a2c8f7b0e614980f9
Jamie Y <cyeh12@hawk.iit.edu>
2025-06-15 15:44:16 -0700

    conditional rendering

--- React-App/src/components/ListGroup.tsx
+++ React-App/src/components/ListGroup.tsx
@@ -1,12 +1,12 @@
 function ListGroup() {
 	let items = ["Town 1", "Town 2", "Town 3"]
 	// items = [];
-	if (items.length===0) {
-		return <p>no item founds</p>
-	}
+
 	return( 
 		<>
-			<h1>This is List group title</h1>	
+			<h1>This is List group title</h1>
+			{items.length === 0 ? <p>Items not found</p> : null}
+			{items.length === 0 && <p>Items not found</p>}	
 			<ul className="list-group">
 				{items.map(item => <li key={item} className="list-group-item">{item}</li>)}
 			</ul>
