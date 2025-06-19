75844f590aa57f2c2b796ee08be0cbccf825b1d8
Jamie Y <cyeh12@hawk.iit.edu>
2025-06-19 15:18:18 -0700

    Use mouse click event to log something in the console

--- React-App/src/components/ListGroup.tsx
+++ React-App/src/components/ListGroup.tsx
@@ -1,17 +1,25 @@
-function ListGroup() {
-	let items = ["Town 1", "Town 2", "Town 3"]
-	// items = [];
+import type { MouseEvent } from "react";
 
-	return( 
-		<>
-			<h1>This is List group title</h1>
-			{items.length === 0 ? <p>Items not found</p> : null}
-			{items.length === 0 && <p>Items not found</p>}	
-			<ul className="list-group">
-				{items.map(item => <li key={item} className="list-group-item">{item}</li>)}
-			</ul>
-		</>	
-	);
+function ListGroup() {
+  let items = ["Town A", "Town B", "Town C", "Town D", "Town E"];
+
+  // event handler
+  const handleClick = (event: MouseEvent) => console.log(event);
+
+  return (
+    <>
+      <h1>This is List group title</h1>
+      {items.length === 0 ? <p>Items not found</p> : null}
+      {items.length === 0 && <p>Items not found</p>}
+      <ul className="list-group">
+        {items.map((item) => (
+          <li key={item} className="list-group-item" onClick={handleClick}>
+            {item}
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 }
 
 export default ListGroup;
