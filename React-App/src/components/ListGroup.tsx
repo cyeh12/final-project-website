13aa96852cdd908aa3ef239e7c0757eb280d5a28
Jamie Y <cyeh12@hawk.iit.edu>
2025-06-19 16:37:04 -0700

    Set a selected item index and only the selected one is active(highlighted)

--- React-App/src/components/ListGroup.tsx
+++ React-App/src/components/ListGroup.tsx
@@ -2,6 +2,7 @@ import type { MouseEvent } from "react";
 
 function ListGroup() {
   let items = ["Town A", "Town B", "Town C", "Town D", "Town E"];
+  let selectedItem = 0;
 
   // event handler
   const handleClick = (event: MouseEvent) => console.log(event);
@@ -12,8 +13,16 @@ function ListGroup() {
       {items.length === 0 ? <p>Items not found</p> : null}
       {items.length === 0 && <p>Items not found</p>}
       <ul className="list-group">
-        {items.map((item) => (
-          <li key={item} className="list-group-item" onClick={handleClick}>
+        {items.map((item, index) => (
+          <li
+            key={item}
+            className={
+              selectedItem === index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
+            onClick={handleClick}
+          >
             {item}
           </li>
         ))}
